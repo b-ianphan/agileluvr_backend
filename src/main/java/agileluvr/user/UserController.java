eeb157391a7395d1f7258dd5f4b7072b68f8e31b
b-ianphan <billyphan232@gmail.com>
2023-09-07 14:40:09 -0700

    Added user functionality with projects

--- src/main/java/agileluvr/user/UserController.java
+++ src/main/java/agileluvr/user/UserController.java
@@ -5,12 +5,14 @@ import agileluvr.common.documents.UserDocument;
 import agileluvr.common.models.BasicUserModel;
 import agileluvr.common.models.ReducedUserModel;
 import agileluvr.common.models.SecureUserModel;
-import agileluvr.common.errors.NotAuthorizedError;
-import agileluvr.common.errors.UserNotFoundError;
+import agileluvr.common.errors.user.NotAuthorizedError;
+import agileluvr.common.errors.user.UserNotFoundError;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Null;
 
 
 @RestController
@@ -24,6 +26,18 @@ public class UserController {
 
     public UserController(UserRepository users){ this.users = users;}
 
+    @ApiOperation(value = "Sign up", notes = "Sign up with a username and password")
+    @PostMapping("/sign_up")
+    public ReducedUserModel createUser(@RequestBody SecureUserModel user){
+        if (user.getApiPassword() != null && user.getApiPassword().equals(this.apiPassword)) {
+            UserDocument savedUser = users.saveUsernameAndPassword(user.getUsername(), user.getPassword());
+            return ReducedUserModel.builder()
+                    .username(savedUser.getUsername())
+                    .id(savedUser.getId())
+                    .build();
+        } else throw new NotAuthorizedError();
+    }
+
     @ApiOperation(value = "Sign in", notes = "Get the User ID with a username and password")
     @GetMapping("/sign_in")
     public ReducedUserModel getUser(@RequestBody BasicUserModel user){
@@ -36,16 +50,34 @@ public class UserController {
                 .build();
     }
 
-    @ApiOperation(value = "Sign up", notes = "Sign up with a username and password")
-    @PostMapping("/sign_up")
-    public ReducedUserModel createUser(@RequestBody SecureUserModel user){
-        if (user.getApiPassword() != null && user.getApiPassword().equals(this.apiPassword)) {
-            UserDocument savedUser = users.saveUsernameAndPassword(user.getUsername(), user.getPassword());
-            return ReducedUserModel.builder()
-                    .username(savedUser.getUsername())
-                    .id(savedUser.getId())
-                    .build();
-        } else throw new NotAuthorizedError();
+
+    @ApiOperation(value = "Assign/Unassign", notes = "Add or remove user to/from project listing")
+    @PutMapping("/{listingID}/{uid}/leave")
+    public UserDocument changeListing(@PathVariable @ApiParam(name = "id of listed project", value = "listed project") String listingID,
+                                      @PathVariable @ApiParam(name = "id of user", value = "user") String uid){
+
+        UserDocument foundUser = users.findById(uid)
+                .orElseThrow(() -> new UserNotFoundError(uid));
+
+        foundUser.setActiveProjectID(listingID);
+
+        return users.save(foundUser);
+
+
+    }
+
+
+    @PutMapping("/{projectID}/{uid}/complete")
+    public UserDocument addCompletedProject(@PathVariable @ApiParam(name = "id of project", value = "completed project") String projectID,
+                                            @PathVariable @ApiParam(name = "id of user", value = "user") String uid){
+
+        UserDocument foundUser = users.findById(uid)
+                .orElseThrow(() -> new UserNotFoundError(uid));
+
+        foundUser.getPreviousProjects().add(projectID);
+
+        return users.save(foundUser);
+
     }
 
 }
